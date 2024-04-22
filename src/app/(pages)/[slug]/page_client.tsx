2b00ce93a2f4f746d079cda992f4accca2dc210b
Kristijan Vidović <kristijan.vidovic@pointsyncc.com>
2024-04-22 15:02:42 +0200

    feat: Added favicon

--- src/app/(pages)/[slug]/page_client.tsx
+++ src/app/(pages)/[slug]/page_client.tsx
@@ -2,10 +2,9 @@
 import { useLivePreview } from '@payloadcms/live-preview-react'
 import React from 'react'
 
-import { Header } from '@/components/Header'
+import { Blocks } from '@/components/Blocks'
 import { Header as HeaderType, Page } from '@/payload/payload-types'
 import HeroSection from 'src/app/components/common/hero-section'
-import { Blocks } from '@/components/Blocks'
 
 // Fetch the page in a server component, pass it to the client component, then thread it through the hook
 // The hook will take over from there and keep the preview in sync with the changes you make
@@ -13,26 +12,22 @@ import { Blocks } from '@/components/Blocks'
 export const PageClient: React.FC<{
   page: Page
   header: HeaderType
-}> = ({ page: initialPage, workingSundays, header }) => {
+}> = ({ page: initialPage, header }) => {
   const { data } = useLivePreview<Page>({
     initialData: initialPage,
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL,
     depth: 1,
   })
 
-  const bgURL = typeof data.featuredImage === 'string' ? data.featuredImage : data.featuredImage?.url
+  const bgURL =
+    typeof data.featuredImage === 'string' ? data.featuredImage : data.featuredImage?.url
 
   return (
     <React.Fragment>
-     {/* <Header showWorkingSundays={data.showWorkingSundays} workingSundays={workingSundays.sunday} header={header} /> */}
-     <HeroSection pageTitle={data.title} bgURL={bgURL} />
-     {/* <Hero {...data.hero} /> */}
-      <Blocks
-        blocks={data.layout}
-        disableTopPadding={
-          !data.hero || data.hero?.type === 'none' || data.hero?.type === 'lowImpact'
-        }
-      /> 
+      {/* <Header showWorkingSundays={data.showWorkingSundays} workingSundays={workingSundays.sunday} header={header} /> */}
+      <HeroSection pageTitle={data.title} bgURL={bgURL} />
+      {/* <Hero {...data.hero} /> */}
+      <Blocks blocks={data.layout} />
     </React.Fragment>
   )
 }
