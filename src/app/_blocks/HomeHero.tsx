66325d901d2e4eb787be668706146879160cf101
Kristijan Vidović <kristijan.vidovic@pointsyncc.com>
2024-04-26 21:15:17 +0200

    feat: Styling changes

--- src/app/_blocks/HomeHero.tsx
+++ src/app/_blocks/HomeHero.tsx
@@ -65,26 +65,38 @@ export const HomeHero = ({ heading, description, image, cta, galleryLink }: any)
   const galleryLinkHref = getPayloadLinkHref(galleryLink)
 
   return (
-    <Section className="lg:mt-28" containerProps={{ className: 'flex flex-col mt-8 lg:mt-0 lg:flex-row justify-between items-stretch space-y-6 space-y-reverse lg:space-y-0' }}>
+    <Section
+      className="lg:mt-28"
+      containerProps={{
+        className:
+          'flex flex-col mt-8 lg:mt-0 lg:flex-row justify-between items-stretch space-y-6 space-y-reverse lg:space-y-0',
+      }}
+    >
       <div className="lg:w-1/2 lg:basis-1/2  space-y-6 flex flex-col pr-4 ">
         <Heading level={1}>{heading}</Heading>
         <Text level={1} className="lg:max-w-[33.5625rem]">
           {description}
-        <div className="lg:mt-36 space-x-5 hidden lg:flex">
-          <Link
-            href={ctaHref}
-            className="bg-primary hover:!text-secondary !text-primary-foreground px-7 py-2.5 rounded-sm inline-block w-fit whitespace-nowrap"
-          >
-            {cta.label}
-          </Link>
-          <LinkWithIcon href={galleryLinkHref} icon={MdArrowOutward} iconPosition='append' className='w-fit whitespace-nowrap'>
-            {galleryLink.label}
-          </LinkWithIcon>
-        </div>
+          <div className="lg:mt-36 space-x-5 hidden lg:flex">
+            <Link
+              href={ctaHref}
+              className="bg-primary hover:!text-secondary !text-primary-foreground px-7 py-2.5 rounded-sm inline-block w-fit whitespace-nowrap"
+            >
+              {cta.label}
+            </Link>
+            <LinkWithIcon
+              href={galleryLinkHref}
+              icon={MdArrowOutward}
+              iconPosition="append"
+              className="w-fit whitespace-nowrap"
+            >
+              {galleryLink.label}
+            </LinkWithIcon>
+          </div>
         </Text>
       </div>
       <div className="lg:w-1/2 lg:basis-1/2 max-w-[550px] lg:block flex justify-center pt-8 lg:pt-0*">
         <Image
+          priority
           responsive={false}
           src={image.url}
           width={image.width}
@@ -93,16 +105,21 @@ export const HomeHero = ({ heading, description, image, cta, galleryLink }: any)
         />
       </div>
       <div className="lg:!mt-auto flex lg:hidden space-x-5">
-          <Link
-            href={ctaHref}
-            className="bg-primary hover:!text-secondary !text-primary-foreground px-7 py-2.5 rounded-sm inline-block w-fit whitespace-nowrap"
-          >
-            {cta.label}
-          </Link>
-          <LinkWithIcon href={galleryLinkHref} icon={MdArrowOutward} iconPosition='append' className='w-fit whitespace-nowrap'>
-            {galleryLink.label}
-          </LinkWithIcon>
-        </div>
+        <Link
+          href={ctaHref}
+          className="bg-primary hover:!text-secondary !text-primary-foreground px-7 py-2.5 rounded-sm inline-block w-fit whitespace-nowrap"
+        >
+          {cta.label}
+        </Link>
+        <LinkWithIcon
+          href={galleryLinkHref}
+          icon={MdArrowOutward}
+          iconPosition="append"
+          className="w-fit whitespace-nowrap"
+        >
+          {galleryLink.label}
+        </LinkWithIcon>
+      </div>
     </Section>
   )
 }
